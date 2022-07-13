1df530b2fad5084f177bd246095c2ac939563fe7
diesel <jpedro.diesel@gmail.com>
2022-07-13 18:05:59 -0300

    (fix): fixing mongo model

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,5 +1,5 @@
 import { Document, Model as M } from 'mongoose';
-import Model from '.';
+import { Model } from '../interfaces/ModelInterface';
 
 abstract class MongoModel<T> implements Model<T> {
   constructor(protected model: M<T & Document>) { }
@@ -10,6 +10,11 @@ abstract class MongoModel<T> implements Model<T> {
 
   readOne = async (id: string): Promise<T | null> =>
     this.model.findOne({ _id: id });
+
+  update = async (id: string, obj: T): Promise<T | null> => 
+    this.model.findOneAndUpdate({ _id: id }, obj);
+
+  delete = async (id: string) => this.model.findOneAndDelete({ _id: id });
 }
 
 export default MongoModel;
