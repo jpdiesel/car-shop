d02281a486faf92d1717954200ccf40abb3474c7
diesel <jpedro.diesel@gmail.com>
2022-07-13 19:13:38 -0300

    (feat): create car service

---
+++ src/services/CarService.ts
@@ -0,0 +1,19 @@
+import Service, { ServiceError } from '.';
+import { Car, CarType } from '../interfaces/CarInterface';
+import CarModel from '../models/CarModel';
+
+class CarService extends Service<CarType> {
+  constructor(model = new CarModel()) {
+    super(model);
+  }
+
+  create = async (obj: CarType): Promise<CarType | ServiceError | null> => {
+    const parsed = Car.safeParse(obj);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.create(obj);
+  };
+}
+
+export default CarService;
